58434e774cdd030fb2c0817eed9d315605497a8b
stephenking <stephenking@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-06-19 19:53:27 +0000

    Fixed bug #10050: Using deprecated XLLfile stuff (Thanks to Susanne Moog and Frank Kroeber)
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/stg_clearcache/trunk@48901 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,6 +4,6 @@ if(!defined('TYPO3_MODE'))   die('Access denied.');
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearCachePostProc'][] = 'EXT:stg_clearcache/class.tx_stgclearcache.php:&tx_stgclearcache->clearCachePostProc';
 
 // override clear_cache label
-$TYPO3_CONF_VARS['BE']['XLLfile']['EXT:lang/locallang_core.php'] = t3lib_extMgm::extPath($_EXTKEY) . 'locallang.xml';
+$TYPO3_CONF_VARS['SYS']['locallangXMLOverride']['EXT:lang/locallang_core.php'][] = t3lib_extMgm::extPath($_EXTKEY) . 'locallang.xml';
 
 ?>
